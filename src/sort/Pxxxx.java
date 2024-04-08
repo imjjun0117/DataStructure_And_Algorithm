f33ed46a71a8518fc0364e3a778c230c734847f7
황성준 <imjjun0117@naver.com>
2024-04-08 23:50:11 +0900

    String 배열 중 숫자만 골라 정렬하기

---
+++ src/sort/Pxxxx.java
@@ -0,0 +1,38 @@
+package sort;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+
+
+//배열 1,5,3,x,10 을 숫자만 나오게 순서대로 정렬하기
+public class Pxxxx {
+
+    public static void main(String[] args) throws IOException {
+
+        String[] strArr = {"1", "5", "3", "x", "10"};
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+
+        for(String str : strArr){
+
+            try{
+
+                numbers.add(Integer.parseInt(str));
+
+            }catch(Exception e){
+
+                continue;
+
+            }//end catch
+
+        }//end for
+
+
+        Collections.sort(numbers);
+
+        for(int i = 0; i < numbers.size(); i++){
+            System.out.println(numbers.get(i));
+        }
+
+    }
+}
